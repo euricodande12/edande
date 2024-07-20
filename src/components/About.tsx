abfa35aae891af5fcf9670675d630c9373770244
euricodande12 <euricodande12@gmail.com>
2024-07-20 22:20:50 +0200

    Experience done.

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -29,7 +29,7 @@ const ServiceCard: React.FC<ServiceCardProps> = ({ index, title, icon }) => {
         <div
         className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col">
           <img src={icon} alt={title} className="w-16 h-16 object-contain" />
-          <h3 className="text-white text-[20px] font-bold text-center">{title}</h3>
+          <h3 className="text-white text-[16px] font-bold text-center">{title}</h3>
         </div>
       </motion.div>
     </Tilt>
